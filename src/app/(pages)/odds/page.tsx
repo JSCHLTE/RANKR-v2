635f2b249cbafdf5cec33c583f299d96c39a857c
Jordan Schulte <email.jordanschulte@gmail.com>
2026-09-15 13:20:11 -0400

    Changed week to week 2

--- src/app/(pages)/odds/page.tsx
+++ src/app/(pages)/odds/page.tsx
@@ -1,2 +1,2 @@
 import { redirect } from "next/navigation";
-export default function OddsPage() { redirect("/odds/nfl/2026/week-1"); }
+export default function OddsPage() { redirect("/odds/nfl/2026/week-2"); }
